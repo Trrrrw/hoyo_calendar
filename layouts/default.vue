0ac14fd6b61e46bc627de2fdfed5ed1b17021e2a
Trrrrw <wzhhenry@qq.com>
2025-08-03 12:53:04 +0800

    Update 2025-08-03

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,11 @@
 </script>
 
 <template>
+
+    <Head>
+        <Title>米哈游游戏日历订阅</Title>
+        <Meta name="description" content="自动同步《原神》《星穹铁道》《绝区零》的官方活动日程到您的日历应用" />
+    </Head>
     <a-layout style="background-color: #ffffff;">
         <a-layout-content style="min-height: 100vh;">
             <slot />
