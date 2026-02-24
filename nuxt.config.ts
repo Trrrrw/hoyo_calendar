c6537678c94cf8f8d75c4293f6789b0d5d18053d
Trrrrw <wzhhenry@qq.com>
2026-02-24 22:39:02 +0800

    feat(路由规则): 为ICS接口添加SSR禁用和自定义头
    添加对/api/**/*.ics路由的特殊处理，禁用服务端渲染并设置自定义头

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     modules: ['@ant-design-vue/nuxt'],
-    css: ['~/assets/css/custon.scss']
+    css: ['~/assets/css/custon.scss'],
+    routeRules: {
+        '/api/**/*.ics': {
+            ssr: false,
+            headers: { 'x-skip-nuxt': 'true' }
+        }
+    }
 })
